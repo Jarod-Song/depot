443cf402cef34a1d316914f33b9b48cd7c0c5911
jarod.song <jarod.song@gree.co.jp>
2014-03-16 21:44:53 +0900

    finished F5

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -17,11 +17,11 @@ class ProductTest < ActiveSupport::TestCase
 
     product.price = -1
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.0.1"], product.errors[:price]
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
 
     product.price = 0
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.0.1"], product.errors[:price]
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
 
     product.price = 1
     assert product.valid?
